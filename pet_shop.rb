67fdcd953773f8a871be90f005b8a09ee7a664e8
Richard Ramson <richard.ramson.jam@gmail.com>
2018-04-13 13:38:07 +0100

    and remove tests

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,15 @@
 def pet_shop_name(shop)
   return shop[:name]
 end
+
+def total_cash(total)
+  return total [:admin][:total_cash]
+end
+
+def add_or_remove_cash(total_cash, add_cash)
+  total_cash[:admin][:total_cash] += (add_cash)
+end
+
+def  
+
+end
